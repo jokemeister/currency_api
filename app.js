1ef414723fe243065addaf21c0da6fc606a4e0ca
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-10-15 15:21:28 +0300

    add proper request

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.get(`/`, (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  console.log();
 
-  const p24apiData = axios(`https://api.privatbank.ua/p24api/exchange_rates?json&date=${req.query.date}`)
-  
-  p24apiData.then(data => res.json(data.data));
-  
-})
+  const p24apiData = axios(
+    // `https://api.privatbank.ua/p24api/exchange_rates?json&date=${req.query.date}`
+    'https://api.privatbank.ua/p24api/exchange_rates?json&date=15.10.2022'
+  );
+
+  p24apiData.then((data) => {
+    console.log(data.data);
+    res.json(data.data);
+  });
+});
 
 module.exports = app;
