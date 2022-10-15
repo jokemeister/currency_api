a10a6fb44068038de0f13ad23242e7ac0c22fec9
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-10-15 14:55:42 +0300

    create api for using privat24api

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express()
+
+app.get(`/`, (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const p24apiData = axios(`https://api.privatbank.ua/p24api/exchange_rates?json&date=${req.query.date}`)
+  
+  p24apiData.then(data => res.send(data.data));
+  
+})
+
+module.exports = app;
