05d7b7b53efee9864f7827ec9acd6e9c3e504ee8
Daniil Dubrovskyi <ddi.dubrovskyi@gmail.com>
2022-10-15 15:11:39 +0300

    add cors middleware

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+app.use(cors())
 
 app.get(`/`, (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const p24apiData = axios(`https://api.privatbank.ua/p24api/exchange_rates?json&date=${req.query.date}`)
   
-  p24apiData.then(data => res.send(data.data));
+  p24apiData.then(data => res.json(data.data));
   
 })
 
